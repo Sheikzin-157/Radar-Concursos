2a36604aaf197371b7c2e7b5bd44696f415fc843
Sheikzin-157 <joan1515cosmo@gmail.com>
2026-08-24 13:57:04 -0300

    feat(competitions): register competition detail use case

--- backend/src/competitions/competitions.module.ts
+++ backend/src/competitions/competitions.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { COMPETITION_REPOSITORY } from './application/ports/competition.repository';
+import { GetCompetitionUseCase } from './application/use-cases/get-competition.use-case';
 import { ListCompetitionsUseCase } from './application/use-cases/list-competitions.use-case';
 import { PostgresCompetitionRepository } from './infrastructure/persistence/postgres-competition.repository';
 import { CompetitionsController } from './presentation/competitions.controller';
@@ -7,6 +8,7 @@ import { CompetitionsController } from './presentation/competitions.controller';
 @Module({
   controllers: [CompetitionsController],
   providers: [
+    GetCompetitionUseCase,
     ListCompetitionsUseCase,
     {
       provide: COMPETITION_REPOSITORY,
